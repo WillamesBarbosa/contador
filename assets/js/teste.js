9f7d775bf0319887d879ad25efaa6fa3e8de5dbc
WillamesBarbosa <willamessilva1999@gmail.com>
2022-05-24 10:42:00 -0300

    Commit inicial

---
+++ assets/js/teste.js
@@ -0,0 +1,27 @@
+var numeroAtualPegar = document.getElementById('numeroAtual');
+var numeroAtual = 0;
+var corBotao = document.getElementById('diminuir');
+
+
+function incrementar(){
+    numeroAtual = numeroAtual + 1;
+    numeroAtualPegar.innerHTML = numeroAtual;
+    if(numeroAtual > 0){
+        corBotao.style.backgroundColor = '#ff0043';
+    }
+}
+
+function decrementar(){
+    if(numeroAtual === 0){
+        numeroAtual = 0
+        corBotao.style.backgroundColor = 'gray';
+    }else{
+        numeroAtual = numeroAtual - 1;
+        numeroAtualPegar.innerHTML = numeroAtual;
+        corBotao.style.backgroundColor = '#ff0043';
+    }
+    if(1 > numeroAtual){
+        corBotao.style.backgroundColor = 'gray';
+        corBotao.style.border = 'gray';
+    }
+}
